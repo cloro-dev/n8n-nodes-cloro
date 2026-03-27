0f37773d4b48a0af102b0a3346f60cbaa237a8d7
rbatista191 <ricardo.serodio.batista@gmail.com>
2026-03-27 04:24:16 -0700

    fix: Remove final period from description per linting rules

--- nodes/Cloro/descriptions.ts
+++ nodes/Cloro/descriptions.ts
@@ -129,7 +129,7 @@ export const descriptions: INodeProperties[] = [
 		displayName: 'Country Code Name or ID',
 		name: 'country',
 		type: 'options',
-		description: 'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>.',
+		description: 'Choose from the list, or specify an ID using an <a href="https://docs.n8n.io/code/expressions/">expression</a>',
 		typeOptions: {
 			loadOptionsMethod: 'getCountries',
 		},
